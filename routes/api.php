b8815d23fd15ef891031fb574d38c0f90945988f
theprinceofcodes <prince@underthehammer.com>
2023-01-30 00:28:34 +0100

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
+require __DIR__ . '/api/users.php';
 
-use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,28 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::resource('users', UserController::class);
 
-Route::get('/users', function (Request $request){
-
-});
-
-Route::get('/users/{user}', function (Request $request,User $user){
-
-});
-
-Route::post('/users', function (Request $request){
-
-});
-
-Route::patch('/users', function (Request $request){
-
-});
-
-Route::delete('/users/{user}', function (User $user){
-
-});
-
-Route::get('/users/{id}', function ($id){
-    
-});
---
+++ routes/api/users.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+// Route::resource('users', UserController::class);
+
+Route::middleware(['auth'])->name('users.')->group(function(){
+    Route::get('/users', [UserController::class, 'index'])->name('index');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
+    Route::post('/users', [UserController::class, 'store'])->name('store');
+    Route::patch('/users', [UserController::class, 'update'])->name('update');
+    Route::delete('/users', [UserController::class, 'destroy'])->name('delete');
+});
+
